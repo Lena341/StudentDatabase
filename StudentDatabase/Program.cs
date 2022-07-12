6ab90602edf04a0543106dc0968bb0af07ea1aa5
Eleni K <71977337+Lena341@users.noreply.github.com>
2022-07-12 21:40:21 +0300

    Update Program.cs

--- StudentDatabase/Program.cs
+++ StudentDatabase/Program.cs
@@ -7,23 +7,20 @@ namespace StudentDatabase
     {
         static void Main(string[] args)
         {
-            SqlConnection connection = new SqlConnection(); //SqlConnection is an ADO.NET class that manages connection with SQL Server DB.
+            SqlConnection connection = new SqlConnection(); 
             try
             {
-                //Here we try to create a connection to the DB. The connection will be of type Windows Authentication
                 connection.ConnectionString = "Integrated Security=true;Initial Catalog=StudentDB;Data Source=server\\SQLExpress";
                 connection.Open();
                 Console.Write("Please enter a Last Name: ");
-                string studentLastName = Console.ReadLine(); //The student's last name will be saved in this string
-                SqlCommand command = new SqlCommand(); //SqlCommand is used for queries in a DB. It executes an order from an SQL command.
+                string studentLastName = Console.ReadLine(); 
+                SqlCommand command = new SqlCommand(); 
                 command.Connection = connection;
                 command.CommandText = "SELECT Name, LastName,Semester,Direction FROM Students WHERE LastName='" + studentLastName + "'";
                 Console.WriteLine("About to execute: {0}\n\n", command.CommandText);
-                SqlDataReader dataReader = command.ExecuteReader(); //With dataReader we can show the data from the DB
+                SqlDataReader dataReader = command.ExecuteReader(); 
                 while(dataReader.Read())
                 {
-                    //The while loop reads the lines from the dataReader variable.Read() method gets the next line from the DB.
-                    //We use the GetXXXX() methods to get the info of every collumn. The 0,1,2,3 show which collumn will be next to read. 
                     string name = dataReader.GetString(0);
                     string lastName = dataReader.GetString(1);
                     int semester = dataReader.GetInt32(2);
